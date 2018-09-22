58da6dd99984d3ae18abc5fd0f5a6db7b1585fd2
Jarek Kajko <jaruto7@gmail.com>
2018-09-23 01:10:07 +0200

    Create other class component in script.js

--- js/script.js
+++ js/script.js
@@ -19,11 +19,47 @@ var Movie = React.createClass( {
         render: function() {
         return (
             React.createElement( 'div', {},
-                React.createElement( 'ul', { key: movie.id }, ),
+        React.createElement( 'ul', { key: movie.id }, ),
                 React.createElement( 'li',  {}, this.props.movie.name )
                 )
             )    
         },
+});
+
+var MovieTitle = React.createClass( {
+    propTypes: {
+        title: React.PropTypes.object.isRequired,
+    },
+        render: function() {
+            React.createElement( 'div', {},
+        React.createElement( 'ul', {}, ),
+                React.createElement( 'li', {}, this.props.title.name )
+            )
+        }
+});
+
+var MovieDescription = React.createClass( {
+    propTypes: {
+        desc: React.PropTypes.object.isRequired,
+    },
+        render: function() {
+            React.createElement( 'div', {},
+                React.createElement( 'ul', {}, ),
+                React.createElement( 'li', {}, this.props.desc.name )
+            )
+        }
+});
+
+var MovieList = React.createClass( {
+    propTypes: {
+        list: React.PropTypes.object.isRequired,
+    },
+        render: function() {
+            React.createElement( 'div', {},
+        React.createElement( 'ul', {}, ),
+                React.createElement( 'li', {}, this.props.list.name )
+            )
+        }
 });
 
 var image = {
@@ -31,11 +67,17 @@ var image = {
     src: './images/kotki-dlugowlose.jpg'
 };
 
-
+var element = React.createElement( GalleryItem, { image: image } )
+    ReactDOM.render( element, document.getElementById( 'app' ) );
 
 var element2 = React.createElement( Movie, { key: movie.id, movie: movie } )
     ReactDOM.render( element2, document.getElementById( 'app' ) );
 
-var element = React.createElement( GalleryItem, { image: image } );
-        ReactDOM.render( element, document.getElementById( 'app' ) );
+var element3 = React.createElement( MovieTitle, { title: title } )
+    ReactDOM.render( element3, document.getElementById( 'app' ) );
 
+var element4 = React.createElement( MovieDescription, { desc: desc } )
+    ReactDOM.render( element4, document.getElementById( 'app' ) );
+    
+var element5 = React.createElement( MovieList, { list: list } )
+    ReactDOM.render( element5, document.getElementById( 'app' ) );    
